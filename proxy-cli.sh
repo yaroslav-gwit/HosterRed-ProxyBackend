50e89682d3ac00dc2618fb42afa8877d1d06a10a
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-06-27 21:16:19 +0100

    CI

--- proxy-cli.sh
+++ proxy-cli.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-cd /opt/hoster_red_proxy/
-source bin/activate
-python3 proxy-cli
+if [[ ! -z $1 ]]; then
+    cd /opt/hoster_red_proxy/
+    source bin/activate
+    python3 proxy-cli $@
+else
+    cd /opt/hoster_red_proxy/
+    source bin/activate
+    python3 proxy-cli --help
