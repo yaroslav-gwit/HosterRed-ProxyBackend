ed73eea9788bee1af2d5bfb63235d910b485b499
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-06-27 21:04:19 +0100

    CI

--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
-mkdir /opt/hoster_red_proxy
-git clone https://github.com/yaroslav-gwit/HosterRed-ProxyBackend.git /opt/hoster_red_proxy/
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root!"
+    exit 1
+fi
+
+if [[ ! -d /opt/hoster_red_proxy ]]; then
+    mkdir /opt/hoster_red_proxy
+    git clone https://github.com/yaroslav-gwit/HosterRed-ProxyBackend.git /opt/hoster_red_proxy/
+    cat /opt/hoster_red_proxy/proxy-cli.sh > /bin/proxy-cli
+    chmod +x /bin/proxy-cli
+    cd /opt/hoster_red_proxy/
+    apt install python3-pip python3-venv
+    python3 -m venv .
+    source bin/activate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt
+    echo "Proxy Manager has been installed!"
+else
+    git pull -C /opt/hoster_red_proxy/
+    cat /opt/hoster_red_proxy/proxy-cli.sh > /bin/proxy-cli
+    chmod +x /bin/proxy-cli
+    echo "Proxy Manager has been upgraded!"
+fi
--- proxy-cli.sh
+++ proxy-cli.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+cd /opt/hoster_red_proxy/
+source bin/activate
+python3 proxy-cli
